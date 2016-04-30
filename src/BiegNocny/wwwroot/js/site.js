f5edf3702cc03ab9588a6be82e501bc42e6ca629
LukaszKubiak <kubiak.lukasz1993@gmail.com>
2016-04-30 21:19:32 +0200

    Map

--- src/BiegNocny/wwwroot/js/site.js
+++ src/BiegNocny/wwwroot/js/site.js
@@ -19,3 +19,36 @@
         img.show();
     })
 })();
+
+
+(function () {
+    $(window).scroll(function () {
+        var top = $(window).scrollTop();
+        var MapDiv = $("#MapDiv");
+        if (MapDiv.height() + top < $("#AddPointForm").height())
+            $("#MapDiv").stop().animate({ "marginTop": ($(window).scrollTop()) + "px" }, "slow");
+        
+    });
+    var map = new GMaps({
+        div: '#map',
+        lat: 52.408333,
+        lng: 16.934167,
+        width: '100%',
+        height: '600px',
+        zoom: 12,
+        click: function (event) {
+            var LatInput = $("#Lat");
+            var LongInput = $("#Long");
+            var lat = event.latLng.lat();
+            var lng = event.latLng.lng();
+            LatInput.attr("value", lat);
+            LongInput.attr("value", lng);
+        },
+        zoomControl: true,
+        zoomControlOpt: {
+            style: 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl: false
+    });
+})();
