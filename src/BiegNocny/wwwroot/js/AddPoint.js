4fcb40af2857ae2a392d732efcf43e07f8e756f7
LukaszKubiak <kubiak.lukasz1993@gmail.com>
2016-05-02 22:08:28 +0200

    ViewPoints

--- src/BiegNocny/wwwroot/js/AddPoint.js
+++ src/BiegNocny/wwwroot/js/AddPoint.js
@@ -1,45 +1,45 @@
 ﻿(function () {
-    $("input[type=radio][name=optionsRadios]").on("change", function () {
+    $("input[type=radio][name=AddoptionsRadios]").on("change", function () {
         switch ($(this).val()) {
             case 'option1':
-                $("#NextPointBtn").hide();
-                $("#PointImg").hide();
-                $("#NextPointText").show();
+                $("#AddNextPointBtn").hide();
+                $("#AddPointImg").hide();
+                $("#AddText").show();
                 break;
             case 'option2':
-                $("#NextPointText").hide();
-                $("#NextPointBtn").show();
+                $("#AddText").hide();
+                $("#AddNextPointBtn").show();
                 break;
         }
     });
-    $("#Upload").on("click", function () {
-        var img = $("#PointImg");
+    $("#AddUpload").on("click", function () {
+        var img = $("#AddPointImg");
         img.attr("src", "https://d-nm.ppstatic.pl/k/r/21/11/56307d97da126_o.jpg");
         img.attr("width", "300px");
         img.show();
     })
 })();
 
-
 (function () {
     $(window).scroll(function () {
         var top = $(window).scrollTop();
-        var MapDiv = $("#MapDiv");
-        if (MapDiv.height() + top < $("#AddPointForm").height())
-            $("#MapDiv").stop().animate({ "marginTop": ($(window).scrollTop()) + "px" }, "slow");
+        var MapDiv = $("#AddMapDiv");
+        if ($(window).width() > 992)
+           if (MapDiv.height() + top < $("#AddForm").height())
+            $("#AddMapDiv").stop().animate({ "marginTop": ($(window).scrollTop()) + "px" }, "slow");
 
     });
 
     var map = new GMaps({
-        div: '#map',
+        div: '#AddMap',
         lat: 52.408333,
         lng: 16.934167,
         width: '100%',
         height: '500px',
         zoom: 12,
         click: function (event) {
-            var LatInput = $("#Lat");
-            var LongInput = $("#Long");
+            var LatInput = $("#AddLat");
+            var LongInput = $("#AddLong");
             var lat = event.latLng.lat();
             var lng = event.latLng.lng();
             LatInput.attr("value", lat);
---
+++ src/BiegNocny/wwwroot/js/ViewPoints.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    $(window).scroll(function () {
+        var top = $(window).scrollTop();
+        var MapDiv = $("#ViewMapDiv");
+        if ($(window).width() > 992)
+            if (MapDiv.height() + top < $("#ViewForm").height())
+                $("#ViewMapDiv").stop().animate({ "marginTop": ($(window).scrollTop()) + "px" }, "slow");
+
+    });
+
+    var map = new GMaps({
+        div: '#ViewMap',
+        lat: 52.408333,
+        lng: 16.934167,
+        width: '100%',
+        height: '500px',
+        zoom: 12,
+        click: function (event) {
+            var LatInput = $("#ViewLat");
+            var LongInput = $("#ViewLong");
+            var lat = event.latLng.lat();
+            var lng = event.latLng.lng();
+            LatInput.attr("value", lat);
+            LongInput.attr("value", lng);
+        },
+        zoomControl: true,
+        zoomControlOpt: {
+            style: 'SMALL',
+            position: 'TOP_LEFT'
+        },
+        panControl: false
+    });
+})();
+
+(function () {
+    $('#ViewLine').on("change", function () {
+        $('#ViewNumberWrapper').show();
+    })
+    $('#ViewNumberWrapper').on("change", function () {
+        $('#ViewWrapper').show();
+    })
+})();
